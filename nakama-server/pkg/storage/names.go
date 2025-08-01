78b2d06071710b54d5cb2f843ef6bdf860ee66b3
Emmanuel Antico <emmanuel.antico@gmail.com>
2025-07-31 22:49:32 -0300

    reimplementation of world_rpc module

---
+++ nakama-server/pkg/storage/names.go
@@ -0,0 +1,66 @@
+package storage
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/heroiclabs/nakama-common/runtime"
+)
+
+const (
+	STORAGE_COLLECTION_GLOBAL string = "global_data"
+	STORAGE_KEY_NAMES         string = "names"
+)
+
+type GlobalDataNames struct {
+	Names []string `json:"names"`
+}
+
+func ReadNames(ctx context.Context, nk runtime.NakamaModule) ([]string, error) {
+	read := &runtime.StorageRead{
+		Collection: STORAGE_COLLECTION_GLOBAL,
+		Key:        STORAGE_KEY_NAMES,
+	}
+
+	records, err := nk.StorageRead(ctx, []*runtime.StorageRead{read})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	data := []byte(records[0].Value)
+	var names GlobalDataNames
+	if err := json.Unmarshal(data, &names); err != nil {
+		return nil, runtime.NewError("error unmarshaling data", 13)
+	}
+
+	return names.Names, nil
+}
+
+func WriteNames(ctx context.Context, nk runtime.NakamaModule, names []string) error {
+	data := &GlobalDataNames{
+		Names: names,
+	}
+
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return runtime.NewError("error marshaling data", 13)
+	}
+
+	write := &runtime.StorageWrite{
+		Collection:      STORAGE_COLLECTION_GLOBAL,
+		Key:             STORAGE_KEY_NAMES,
+		Value:           string(bytes),
+		PermissionRead:  2,
+		PermissionWrite: 0,
+	}
+
+	_, err = nk.StorageWrite(ctx, []*runtime.StorageWrite{write})
+	if err != nil {
+		return runtime.NewError("error saving data", 13)
+	}
+
+	return nil
+}
---
+++ nakama-server/src/world_rpc/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"github.com/heroiclabs/nakama-common/runtime"
+	"nakama-server/pkg/storage"
+	"slices"
+)
+
+func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
+	if err := initializer.RegisterRpc("get_world_id", getWorldId); err != nil {
+		logger.Error("Unable to register: %v", err)
+		return err
+	}
+	if err := initializer.RegisterRpc("register_character_name", registerCharacterName); err != nil {
+		logger.Error("Unable to register: %v", err)
+		return err
+	}
+	if err := initializer.RegisterRpc("remove_character_name", removeCharacterName); err != nil {
+		logger.Error("Unable to register: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func getWorldId(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	matches, err := nk.MatchList(ctx, 1, false, "", nil, nil, "")
+	if err != nil {
+		return "", err
+	}
+
+	if len(matches) == 0 {
+		m, err := nk.MatchCreate(ctx, "world_control", nil)
+		if err != nil {
+			return "", err
+		}
+		return m, nil
+	}
+
+	return matches[0].MatchId, nil
+}
+
+func registerCharacterName(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	names, err := storage.ReadNames(ctx, nk)
+	if err != nil {
+		return "0", err
+	}
+	if len(names) == 0 {
+		if err := storage.WriteNames(ctx, nk, []string{payload}); err != nil {
+			return "0", err
+		}
+		return "1", nil
+	}
+
+	if slices.Contains(names, payload) {
+		return "0", nil
+	}
+
+	if err := storage.WriteNames(ctx, nk, []string{payload}); err != nil {
+		return "0", err
+	}
+
+	return "1", nil
+}
+
+func removeCharacterName(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	names, err := storage.ReadNames(ctx, nk)
+	if err != nil {
+		return "0", err
+	}
+	if len(names) == 0 {
+		return "1", nil
+	}
+
+	names = slices.DeleteFunc(names, func(s string) bool {
+		return s == payload
+	})
+	if err := storage.WriteNames(ctx, nk, names); err != nil {
+		return "0", err
+	}
+
+	return "1", nil
+}
